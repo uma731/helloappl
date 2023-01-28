ca21bfa16dafb24ff1ce038740383a0a33cb5cef
uma731 <86666586+uma731@users.noreply.github.com>
2023-01-28 14:08:08 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+from flask import Flask,render_template
+app=Flask('__name__')
+@app.route('/'):
+  return render_template('index.html')
+if('__name__' == '__main__'):
+  app.run(server='127.0.0.1',port=8080,debug=True)
